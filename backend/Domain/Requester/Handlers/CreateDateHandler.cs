b9152dc20128b8d47fe3359124d4ff8799ad4984
Alisson Prates Peres <a.pratesperes@gmail.com>
2023-08-23 02:03:17 -0300

    🦺 Validações no CRUD para a entidade: Date
    Implementada a terceira validação na Aplicação para a Entidade: "Date", agora para a solicitação ser realizada, antes a Aplicação hita no Banco de Dados e verifica se o "Prospect" informado na requisição está disponível, visto que o "Date" só pode conter um "Prospect" que esteja como 'Available' durante a sua solicitação. Foi utilizada a mesma estrutura de If/Else e Try/Catch Block assim como feito nas outras partes da Aplicação, visando conter o mesmo padrão de desenvolvimento, no Back-End da Aplicação.

--- backend/Domain/Requester/Handlers/CreateDateHandler.cs
+++ backend/Domain/Requester/Handlers/CreateDateHandler.cs
@@ -7,44 +7,57 @@ using Domain.Shared.Extensions;
 using Domain.Requester.Models;
 using Domain.Requester.Commands;
 using Domain.Requester.Repositories;
+using Domain.Requested.Repositories;
 
     namespace Domain.Requester.Handlers {
         public class CreateDateHandler : IHandler<CreateDateCommand> {
             private readonly IDateRepository _dateRepository;
+            private readonly IProspectRepository _prospectRepository;
             private readonly IUnityOfWork _unityOfWork;
             private readonly ICommandResult<Unit> _commandResult;
-            public CreateDateHandler(IDateRepository dateRepository, IUnityOfWork unityOfWork, ICommandResult<Unit> commandResult) {
+            public CreateDateHandler(IDateRepository dateRepository, IProspectRepository prospectRepository, IUnityOfWork unityOfWork, ICommandResult<Unit> commandResult) {
                 _dateRepository = dateRepository;
+                _prospectRepository = prospectRepository;
                 _unityOfWork = unityOfWork;
                 _commandResult = commandResult;
             }
 
                 public async Task<ICommandResult<Unit>> Handle(CreateDateCommand request, CancellationToken cancellationToken) {
-                    try {
-                        var date = new Date(
-                            name: request.Name,
-                            title: request.Title,
-                            status:
-                                Status.Select(request.Status),
-                            contact: request.Contact,
-                            schedule: request.Schedule,
-                            location: request.Location,
-                            description: request.Description,
-                            displacement:
-                                Displacement.Choose(request.Displacement),
-                            contribution: request.Contribution,
-                            prospectId: request.ProspectId
-                        );
+                    var prospect = await _prospectRepository.Read(request.ProspectId);
 
-                            await _dateRepository.Create(date);
-                            await _unityOfWork.Commit();
+                        if(prospect != null) {
+                            try {
+                                if(!prospect.Status.Available) {
+                                    return _commandResult.BadRequest();
+                                } else {
+                                    var date = new Date(
+                                        name: request.Name,
+                                        title: request.Title,
+                                        status:
+                                            Status.Select(request.Status),
+                                        contact: request.Contact,
+                                        schedule: request.Schedule,
+                                        location: request.Location,
+                                        description: request.Description,
+                                        displacement:
+                                            Displacement.Choose(request.Displacement),
+                                        contribution: request.Contribution,
+                                        prospectId: request.ProspectId
+                                    );
 
-                                return _commandResult.Created();
-                    } catch (Exception) {
-                        _unityOfWork.Rollback();
+                                        await _dateRepository.Create(date);
+                                        await _unityOfWork.Commit();
 
-                            return _commandResult.InternalServerError();
-                    }
+                                            return _commandResult.Created();
+                                }
+                            } catch (Exception) {
+                                _unityOfWork.Rollback();
+
+                                    return _commandResult.InternalServerError();
+                            }
+                        }
+
+                            return _commandResult.NotFound();
                 }
         }
     }
