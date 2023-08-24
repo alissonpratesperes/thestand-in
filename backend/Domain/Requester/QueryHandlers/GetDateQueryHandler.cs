09fe6391c43fb371c9d10262c9c52f9c88fd5c28
Alisson Prates Peres <a.pratesperes@gmail.com>
2023-08-24 03:31:00 -0300

    🚀 Implementação do Caching nos Métodos Get/List para: Date
    Em um Software que utiliza o padrão CQRS (Command Query Responsibility Segregation), no qual se tem componentes separados para lidar com Commands (mutações) e Queries (leituras), o Cache geralmente é implementado no lado das Queries, pois as consultas são operações de leitura com alta demanda. O cache pode melhorar significativamente o desempenho das consultas de leitura, reduzindo a carga no Banco de Dados. É apropriado implementar o Cache dentro do manipulador de consulta, ou seja, dentro dos QueryHandlers para cada Entidade, porque o manipulador de consulta é responsável por lidar com consultas de leitura e buscar dados do Banco de Dados e manipular devidamente esses retornos;
    Iniciando a implementação em: "./backend/Domain/Requester/QueryHandlers" na Classe: 'GetDateQueryHandler', injetada a Interface: 'IMemoryCache' via Injeção de Dependência e manipulado o Caching para durar em memória com um TimeSpan de 5 minutos, através da Função: 'GetOrCreate', que verifica se o Objeto existe na memória e caso contrário o cria autonomamente, essa tratativa também foi implementada de forma assíncrona Async/Await por se tratar de, em caso de Cache Miss - o Cache não conter tais dados a Aplicação hitar no Banco de Dados e retornar, e isto leva um tempo entre todo o fluxo implementado realizar tal processamento;
    Finalizando a implementação em: "./backend/Domain/Requester/QueryHandlers" na Classe: 'ListDateQueryHandler', injetada a Interface: 'IMemoryCache' via Injeção de Dependência e manipulado o Caching para durar em memória com um TimeSpan de 15 minutos, através da Função: 'GetOrCreate', que verifica se o Objeto existe na memória e caso contrário o cria autonomamente, essa tratativa também foi implementada de forma assíncrona Async/Await por se tratar de, em caso de Cache Miss - o Cache não conter tais dados a Aplicação hitar no Banco de Dados e retornar, e isto leva um tempo entre todo o fluxo implementado realizar tal processamento, no Back-End da Aplicação.

--- backend/Domain/Requester/QueryHandlers/GetDateQueryHandler.cs
+++ backend/Domain/Requester/QueryHandlers/GetDateQueryHandler.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Caching.Memory;
+
 using Domain.Shared.Returns;
 using Domain.Shared.QueryHandlers;
 using Domain.Requester.Queries;
@@ -7,16 +9,24 @@ using Domain.Requester.QueryRepositories;
     namespace Domain.Requester.QueryHandlers {
         public class GetDateQueryHandler : IQueryHandler<GetDateQuery, Return<GetDateViewModel>> {
             private readonly IDateQueryRepository _dateQueryRepository;
-            public GetDateQueryHandler(IDateQueryRepository dateQueryRepository) {
+            private readonly IMemoryCache _memoryCache;
+            public GetDateQueryHandler(IDateQueryRepository dateQueryRepository, IMemoryCache memoryCache) {
                 _dateQueryRepository = dateQueryRepository;
+                _memoryCache = memoryCache;
             }
 
                 public async Task<Return<GetDateViewModel>> Handle(GetDateQuery query, CancellationToken cancellationToken) {
-                    var date = await _dateQueryRepository.Get(query.Id);
+                    var cachedResult = await _memoryCache.GetOrCreate("GetOfDate", async entry => {
+                        entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
 
-                        return date != null
-                            ?  Return<GetDateViewModel>.Succeeded(date)
-                            :  Return<GetDateViewModel>.Failure(date);
+                            var date = await _dateQueryRepository.Get(query.Id);
+
+                                return date != null
+                                    ?  Return<GetDateViewModel>.Succeeded(date)
+                                    :  Return<GetDateViewModel>.Failure(date);
+                    });
+
+                        return cachedResult;      
                 }
         }
     }
--- backend/Domain/Requester/QueryHandlers/ListDateQueryHandler.cs
+++ backend/Domain/Requester/QueryHandlers/ListDateQueryHandler.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Caching.Memory;
+
 using Domain.Shared.Returns;
 using Domain.Shared.Paginations;
 using Domain.Shared.QueryHandlers;
@@ -8,23 +10,31 @@ using Domain.Requester.QueryRepositories;
     namespace Domain.Requester.QueryHandlers {
         public class ListDateQueryHandler : IQueryHandler<ListDateQuery, Return<Pagination<ListDateViewModel>>> {
             private readonly IDateQueryRepository _dateQueryRepository;
-            public ListDateQueryHandler(IDateQueryRepository dateQueryRepository) {
+            private readonly IMemoryCache _memoryCache;
+            public ListDateQueryHandler(IDateQueryRepository dateQueryRepository, IMemoryCache memoryCache) {
                 _dateQueryRepository = dateQueryRepository;
+                _memoryCache = memoryCache;
             }
 
                 public async Task<Return<Pagination<ListDateViewModel>>> Handle(ListDateQuery query, CancellationToken cancellationToken) {
-                    var results = await _dateQueryRepository.List(query.Page, query.Length, query.Status);
-                    var count = await _dateQueryRepository.Count(query.Status);
-                    var pagination = new Pagination<ListDateViewModel>(
-                        count: count,
-                        page: query.Page,
-                        length: query.Length,
-                        results: results
-                    );
+                    var cachedResults = await _memoryCache.GetOrCreate("ListOfDate", async entry => {
+                        entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(15);
 
-                        return pagination.Results != null && pagination.Count > 0
-                            ?  Return<Pagination<ListDateViewModel>>.Succeeded(pagination)
-                            :  Return<Pagination<ListDateViewModel>>.Failure(pagination);
+                            var results = await _dateQueryRepository.List(query.Page, query.Length, query.Status);
+                            var count = await _dateQueryRepository.Count(query.Status);
+                            var pagination = new Pagination<ListDateViewModel>(
+                                count: count,
+                                page: query.Page,
+                                length: query.Length,
+                                results: results
+                            );
+
+                                return pagination.Results != null && pagination.Count > 0
+                                    ?  Return<Pagination<ListDateViewModel>>.Succeeded(pagination)
+                                    :  Return<Pagination<ListDateViewModel>>.Failure(pagination);
+                    });
+
+                        return cachedResults;        
                 }
         }
     }
